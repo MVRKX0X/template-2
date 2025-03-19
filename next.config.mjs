2d6e986e084b6e57969eeda7632f02370a9d80e2
MVRKX0X <mark@mgenterprises.io>
2025-03-19 14:10:46 +0100

    open ai fix

--- next.config.mjs
+++ next.config.mjs
@@ -14,27 +14,11 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "replicate.com",
-      },
-      {
-        protocol: "https",
-        hostname: "replicate.delivery",
-      },
-      {
-        protocol: "https",
         hostname: "firebasestorage.googleapis.com",
       },
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://api.openai.com/:path*",
-      },
-    ];
   },
   // Exclude unused API routes from the build
   webpack: (config, { isServer }) => {
